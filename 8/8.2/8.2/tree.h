ccedfd6987151b31b9785f34af518221ece8f3b1
Julia-Berezhnova <julia-berezhnova98@gmail.com>
2015-12-11 19:36:12 +0300

    all homeworks are deleted in order to create branches

--- 8/8.2/8.2/tree.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Node;
-struct Tree;
-Tree *createTree();
-bool belongs(Tree *tree, int number);
-int addNumber(Tree* tree, int number);
-int deleteNumber(Tree* tree, int number);
-void printTree(Tree *tree, int way);
