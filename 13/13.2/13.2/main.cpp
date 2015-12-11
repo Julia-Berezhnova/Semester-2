896b2a65428673928f7ecaa6bd84ad1c9036f179
Julia Berezhnova <juliaberezhnova1998>
2015-12-11 09:55:33 +0300

    13.2 is changed

--- 13/13.2/13.2/main.cpp
+++ 13/13.2/13.2/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <cstdio>
-#include <limits.h>
-
 using namespace std;
 
 const int maxSize = 1024;
@@ -12,118 +10,205 @@ int main()
 	if (input == NULL)
 	{
 		cout << "file not found" << endl;
+		return 1;
 	}
-	else
+	char c = {};
+	int state = 0;
+	char output[maxSize] = {};
+	int size = 0;
+	int table[3][4] = 
 	{
-		char c = {};
-		int state = 0;
-		char output[maxSize] = {};
-		int size = 0;
-		while (c != EOF)
+		/// state 0 state 1 state 2 state 3 
+				1,    1,      2,       0,	/// /
+				0,	  2,	  3,       3,	/// *
+				0,	  0,	  2,	   2	/// other 	
+	};
+	while (c != EOF)
+	{
+		c = getc(input);
+		int previousState = state;
+		if (c == '/')
 		{
-			c = getc(input);
-			switch (state)
+			state = table[0][state];
+		}
+		else if (c == '*')
+		{
+			state = table[1][state];
+		}
+		else
+		{
+			state = table[2][state];
+		}
+		if (previousState == 3 && state == 0)
+		{
+			for (int i = 0; i < size; ++i)
 			{
-			case 0:
-				size = 0;
-				if (c == '/')
+				cout << output[i];
+				output[i] = {};
+			}
+			cout << '/' << endl;
+			size = 0;
+		}
+		if (state == 2)
+		{
+			if (previousState == 1)
+			{
+				if (size < maxSize)
 				{
-					state = 1;
-					if (size < maxSize)
-					{
-						output[size] = c;
-						++size;
-					}
-					else
-					{
-						exit(1);
-					}
-				}
-				else
-				{
-					state = 0;
-				}
-				break;
-			case 1:
-				if (c == '*')
-				{
-					state = 2;
-					if (size < maxSize)
-					{
-						output[size] = c;
-						++size;
-					}
-					else
-					{
-						exit(1);
-					}
-				}
-				else
-				{
-					state = 0;
-				}
-				break;
-			case 2:
-				if (c == '*')
-				{
-					state = 3;
-					if (size < maxSize)
-					{
-						output[size] = c;
-						++size;
-					}
-					else
-					{
-						exit(1);
-					}
-				}
-				else
-				{
-					state = 2;
-					if (size < maxSize)
-					{
-						output[size] = c;
-						++size;
-					}
-					else
-					{
-						exit(1);
-					}
-				}
-				break;
-			case 3:
-				if (c == '/')
-				{
-					state = 0;
-					if (size < maxSize)
-					{
-						output[size] = c;
-						++size;
-					}
-					else
-					{
-						exit(1);
-					}
-					cout << "comment: " << endl;
-					for (int i = 0; i < size; ++i)
-					{
-						cout << output[i];
-						output[i] = {};
-					}
-					cout << endl;
-				}
-				else
-				{
-					state = 2;
-					output[size] = c;
+					output[size] = '/';
 					++size;
 				}
-				break;
+				else
+				{
+					exit(1);
+				}
+			}
+			if (size < maxSize)
+			{
+				output[size] = c;
+				++size;
+			}
+			else
+			{
+				exit(1);
 			}
 		}
-		fclose(input);
+		if (state == 3)
+		{
+			if (size < maxSize)
+			{
+				output[size] = c;
+				++size;
+			}
+			else
+			{
+				exit(1);
+			}
+		}
 	}
+	fclose(input);
+	return 0;
 }
+
+
+
+//int main()
+//{
+//	FILE * input = fopen("input.txt", "r");
+//	if (input == NULL)
+//	{
+//		cout << "file not found" << endl;
+//	}
+//	else
+//	{
+//		char c = {};
+//		int state = 0;
+//		char output[maxSize] = {};
+//		int size = 0;
+//		while (c != EOF)
+//		{
+//			c = getc(input);
+//			switch (state)
+//			{
+//			case 0:
+//				size = 0;
+//				if (c == '/')
+//				{
+//					state = 1;
+//					if (size < maxSize)
+//					{
+//						output[size] = c;
+//						++size;
+//					}
+//					else
+//					{
+//						exit(1);
+//					}
+//				}
+//				else
+//				{
+//					state = 0;
+//				}
+//				break;
+//			case 1:
+//				if (c == '*')
+//				{
+//					state = 2;
+//					if (size < maxSize)
+//					{
+//						output[size] = c;
+//						++size;
+//					}
+//					else
+//					{
+//						exit(1);
+//					}
+//				}
+//				else
+//				{
+//					state = 0;
+//				}
+//				break;
+//			case 2:
+//				if (c == '*')
+//				{
+//					state = 3;
+//					if (size < maxSize)
+//					{
+//						output[size] = c;
+//						++size;
+//					}
+//					else
+//					{
+//						exit(1);
+//					}
+//				}
+//				else
+//				{
+//					state = 2;
+//					if (size < maxSize)
+//					{
+//						output[size] = c;
+//						++size;
+//					}
+//					else
+//					{
+//						exit(1);
+//					}
+//				}
+//				break;
+//			case 3:
+//				if (c == '/')
+//				{
+//					state = 0;
+//					if (size < maxSize)
+//					{
+//						output[size] = c;
+//						++size;
+//					}
+//					else
+//					{
+//						exit(1);
+//					}
+//					for (int i = 0; i < size; ++i)
+//					{
+//						cout << output[i];
+//						output[i] = {};
+//					}
+//					cout << endl;
+//				}
+//				else
+//				{
+//					state = 2;
+//					output[size] = c;
+//					++size;
+//				}
+//				break;
+//			}
+//		}
+//		fclose(input);
+//	}
+//}
 
 
 
@@ -138,12 +223,12 @@ int main()
 
 /// test 2
 /// input
-/// abraxadabra */ */
+/// abrakadabra */ */
 /// output
 
 /// test 3
 /// input
-/// abraxadabra  /*  *   /  */
+/// abrakadabra  /*  *   /  */
 /// output
 /// /*  *   /  */
 
