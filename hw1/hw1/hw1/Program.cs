c88085a94abe645aa687e005ab9c6885d0eb0d0f
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-02-13 20:08:25 +0300

    Homework 1.1 is added

---
+++ hw1/hw1/hw1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hm1_1
+{
+	class MainClass
+	{
+		private static uint Factorial(uint number)
+		{
+			if (number == 0 || number == 1) 
+			{
+				return 1;
+			}
+			return number * Factorial (number - 1);
+		}
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Enter a positive integer number:");
+			uint number;
+			string someString = Console.ReadLine();
+			if (UInt32.TryParse (someString, out number)) 
+			{
+				Console.WriteLine (Factorial (number));
+			} 
+			else 
+			{
+				Console.WriteLine ("Incorrect input");
+			}
+		}
+	}
+}
+
