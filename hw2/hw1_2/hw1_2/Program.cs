8a406cbd30e041a6e2235e43d9192ec4e04dfcde
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-02-13 20:11:17 +0300

    Homework 1.2 is added

---
+++ hw2/hw1_2/hw1_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hm1_2
+{
+	class MainClass
+	{
+		private static uint Fibonacchi(uint number)
+		{
+			if (number == 0 || number == 1) 
+			{
+				return 1;
+			}
+			return Fibonacchi(number - 1) + Fibonacchi(number - 2);
+		}
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Enter a positive integer number:");
+			uint number;
+			string someString = Console.ReadLine();
+			if (UInt32.TryParse (someString, out number)) 
+			{
+				Console.WriteLine (Fibonacchi (number));
+			} 
+			else 
+			{
+				Console.WriteLine ("Incorrect input");
+			}
+		}
+	}
+}
+
