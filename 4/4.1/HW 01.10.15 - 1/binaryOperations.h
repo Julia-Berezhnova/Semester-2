855018163ed2cc86c2cdfcb7803139f19acd38d7
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-02-15 20:05:02 +0300

    Extra stuff is deleted

--- 4/4.1/HW 01.10.15 - 1/binaryOperations.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const int length = sizeof(int) * 8;
-
-void binaryRepresentation(int number, bool binaryNumber[]);
-void binarySum(bool number1[], bool number2[], bool result[]);
-double DecimalRepresentation(bool number[]);
