504f99a2f0aee59af4e3a166fd1afd34a6b1e5fc
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-02-15 20:11:56 +0300

    Extra stuff is deleted

--- 6/6.2/HW 6 - 2/list.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct ListElement;
-struct List;
-
-List* createList();
-void addElement(int x, List* list);
-void deleteElement(int x, List* list);
-void printList(List* list);
--- 6/6.3/HW 6 - 3/list.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct ListElement;
-struct List;
-
-List* createList(int size);
-int survivor(List* list, int m);
-void deleteElement(int x, List* list);
