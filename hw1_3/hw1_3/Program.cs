a60e7099657fb9331cd0b2258bd1438826c330e6
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-02-15 19:55:31 +0300

    Homework 1.3 is added

---
+++ hw1_3/hw1_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hw1_3
+{
+	class MainClass
+	{
+		private static void InsertationSort(int[] array)
+		{
+			for (int i = 1; i < array.Length; i++)
+			{
+				int key = array[i];
+				int j = i;
+				while (j > 0 && key < array[j - 1])
+				{
+					array[j] = array[j - 1];
+					j--;
+				}
+				array[j] = key;
+			}
+		}
+		public static void Main (string[] args)
+		{
+			string[] someString = Console.ReadLine().Split(' ');
+			int[] numbers = new int[someString.Length];
+			for (int i = 0; i < someString.Length; ++i) 
+			{
+				int number;
+				if (Int32.TryParse(someString[i],out number))
+				{
+					numbers [i] = number;
+				}
+				else
+				{
+					Console.WriteLine("Incorrect input");
+					return;
+				}
+			}
+			InsertationSort (numbers);
+			for (int i = 0; i < numbers.Length; ++i)
+			{
+				Console.Write(numbers[i]);
+				Console.Write (" ");
+			}
+		}
+	}
+}
