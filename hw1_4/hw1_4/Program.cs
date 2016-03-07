b15cb52e1b080919025db9aad98e7c0532f35fd0
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-03-07 18:43:52 +0300

    Refactoring is done

--- hw1_4/hw1_4/Program.cs
+++ hw1_4/hw1_4/Program.cs
@@ -6,35 +6,35 @@ namespace hw1_4
 	{
 		private static void GenerateAndPrintArray(int[,] array)
 		{
-			Random rnd = new Random ();
-			Console.WriteLine ("Array: ");
-			for (int i = 0; i < array.GetLength (0); ++i) 
+			Random rnd = new Random();
+			Console.WriteLine("Array: ");
+			for (int i = 0; i < array.GetLength(0); ++i) 
 			{
-				for (int j = 0; j < array.GetLength (1); ++j) 
+				for (int j = 0; j < array.GetLength(1); ++j) 
 				{
-					array [i, j] = rnd.Next (0, 10);
-					Console.Write (array [i, j]);			
-					Console.Write (" ");
+					array[i, j] = rnd.Next(0, 10);
+					Console.Write(array[i, j]);			
+					Console.Write(" ");
 				}
-				Console.WriteLine ();
+				Console.WriteLine();
 			}
 		}
 
 		private static void PrintArray(int[,] array)
 		{
-			if (array.GetLength (0) != array.GetLength (1)) 
+			if (array.GetLength(0) != array.GetLength(1)) 
 			{
-				Console.WriteLine ("Incorrect input. The matrix must be square");
+				Console.WriteLine("Incorrect input. The matrix must be square");
 			}
-			int i = array.GetLength (0) / 2;
-			int j = array.GetLength (1) / 2;
+			int i = array.GetLength(0) / 2;
+			int j = array.GetLength(1) / 2;
 			int step = 1;
 			while (true) 
 			{
 				for (int l = 0; l < step; ++l) 
 				{
-					Console.Write (array [i, j]);
-					Console.Write (" ");
+					Console.Write(array[i, j]);
+					Console.Write(" ");
 					--j;
 				}
 				if (j == -1) 
@@ -43,21 +43,21 @@ namespace hw1_4
 				}
 				for (int l = 0; l < step; ++l) 
 				{
-					Console.Write (array [i, j]);
-					Console.Write (" ");
+					Console.Write(array[i, j]);
+					Console.Write(" ");
 					++i;
 				}
 				++step;
 				for (int l = 0; l < step; ++l) 
 				{
-					Console.Write (array [i, j]);
-					Console.Write (" ");
+					Console.Write(array[i, j]);
+					Console.Write(" ");
 					++j;
 				}
 				for (int l = 0; l < step; ++l) 
 				{
-					Console.Write (array [i, j]);
-					Console.Write (" ");
+					Console.Write(array[i, j]);
+					Console.Write(" ");
 					--i;
 				}
 				++step;
@@ -66,22 +66,22 @@ namespace hw1_4
 
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Enter a dimension (odd number):");
+			Console.WriteLine("Enter a dimension (odd number):");
 			uint dimension;
-			if (!UInt32.TryParse (Console.ReadLine (), out dimension))
-				{
-					Console.WriteLine ("Incorrect input");
-					return;
-				}
+			if (!UInt32.TryParse(Console.ReadLine(), out dimension))
+			{
+				Console.WriteLine("Incorrect input");
+				return;
+			}
 			if (dimension % 2 == 0) 
 			{
-				Console.WriteLine ("Incorrect input");
+				Console.WriteLine("Incorrect input");
 				return;
 			}
 			int[,] array = new int[dimension, dimension];
-			GenerateAndPrintArray (array);
-			Console.WriteLine ("Answer:");
-			PrintArray (array);
+			GenerateAndPrintArray(array);
+			Console.WriteLine("Answer:");
+			PrintArray(array);
 		}
 	}
 }
