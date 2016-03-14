48b29c83d29d74f1d64a9e5cb233829126b29d5a
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-03-15 00:49:19 +0300

    Homework 5.3 is done

---
+++ 5_3/5_3/Fold.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace _3
+{
+    delegate int Function(int acc, int elem);
+
+    class Fold
+    {
+        public int FoldFunction(List<int> list, int initialValue, Function function)
+        {
+            int result = initialValue;
+            foreach (int element in list)
+            {
+                result = function(result, element);
+            }
+            return result;
+        }
+    }
+}
+
---
+++ 5_3/5_3/FoldFunctionTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace _3
+{
+    [TestFixture()]
+    public class FoldFunctionTests
+    {
+        [SetUp()]
+        public void InitialTest()
+        {
+            Fold myFold = new Fold();
+        }
+
+        [Test()]
+        public void SampleTest()
+        {
+            Fold myFold = new Fold();
+            List<int> myList = new List<int>();
+            myList.Add(1);
+            myList.Add(3);
+            myList.Add(2);
+            int result = myFold.FoldFunction(myList, 1, (x, y) => x * y);
+            Assert.AreEqual(6, result);
+        }
+
+        [Test()]
+        public void UselessFuncTest()
+        {
+            Fold myFold = new Fold();
+            List<int> myList = new List<int>() { 6, 9, 8, 7};
+            int result = myFold.FoldFunction(myList, 0, (x, y) => x * y);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test()]
+        public void FiveThousandFuncTest()
+        {
+            Fold myFold = new Fold();
+            List<int> myList = new List<int>() { 9, 8, 7, 6, 5, 4, 3, 2, 1};
+            int result = myFold.FoldFunction(myList, 500, (x, y) => x + y * 100);
+            Assert.AreEqual(5000, result);
+        }
+    }
+}
+
