c54522cadfbe458355a9298c2079c8bfdf9a909e
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-03-14 22:20:24 +0300

    Hopefully, the problem with white spaces is solved

--- hw2_2/hw2_2/List.cs
+++ hw2_2/hw2_2/List.cs
@@ -1,92 +1,92 @@
 ﻿namespace List
 {
-	/// <summary>
-	/// List.
-	/// </summary>
-	class List: IList
-	{
-		private int listSize = 0;
-		private ListElement head = null;
+    /// <summary>
+    /// List.
+    /// </summary>
+    class List: IList
+    {
+        private int listSize = 0;
+        private ListElement head = null;
 
-		private class ListElement
-		{
+        private class ListElement
+        {
             public int Value { get; set; }
 
             public ListElement Next { get; set; }
 
-			public ListElement()
-			{
+            public ListElement()
+            {
                 Value = 0;
                 Next = null;
-			}
+            }
 
-			public ListElement(int value, ListElement next)
-			{
+            public ListElement(int value, ListElement next)
+            {
                 Value = value;
-				Next = next;
-			}
-		}
-			
-		public void AddElement(int element)
-		{
-			if (this.head == null) 
-			{
+                Next = next;
+            }
+        }
+
+        public void AddElement(int element)
+        {
+            if (this.head == null)
+            {
                 this.head = new ListElement(element, null);
-			}
-			else if (this.head.Next == null)
-			{
+            }
+            else if (this.head.Next == null)
+            {
                 this.head.Next = new ListElement(element, null);
-			}	
-			else
-			{
-				ListElement temp = this.head;
-				while (temp.Next != null) 
-				{
-					temp = temp.Next;
-				}
+            }
+            else
+            {
+                ListElement temp = this.head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
                 temp.Next = new ListElement(element, null);
-			}
-			++this.listSize;
-		}
-			
-		public bool IsEmpty() =>  this.head == null;
-			
-		public bool DeleteElement(int element)
-		{
-			if (this.IsEmpty()) 
-			{
-				return false;
-			}
+            }
+            ++this.listSize;
+        }
 
-			ListElement temp = this.head;
-			if (temp.Value == element) 
-			{
-				this.head = this.head.Next;
-				return true;
-			}
-			if (temp.Next != null) 
-			{
-				if (temp.Next.Value == element)
-				{
-					this.head.Next =  this.head.Next.Next;
-					return true;
-				}
-			}
-			else
-			{
-				return false;
-			}
+        public bool IsEmpty() =>  this.head == null;
 
-			while (temp.Next != null) 
-			{
-				if (temp.Next.Value == element)
-				{
-						temp.Next = temp.Next.Next;
-					return true;
-				}
-				temp = temp.Next;
-			}
-			return false;
-		}
-	}
+        public bool DeleteElement(int element)
+        {
+            if (this.IsEmpty())
+            {
+                return false;
+            }
+
+            ListElement temp = this.head;
+            if (temp.Value == element)
+            {
+                this.head = this.head.Next;
+                return true;
+            }
+            if (temp.Next != null)
+            {
+                if (temp.Next.Value == element)
+                {
+                    this.head.Next = this.head.Next.Next;
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            while (temp.Next != null)
+            {
+                if (temp.Next.Value == element)
+                {
+                    temp.Next = temp.Next.Next;
+                    return true;
+                }
+                temp = temp.Next;
+            }
+            return false;
+        }
+    }
 }
