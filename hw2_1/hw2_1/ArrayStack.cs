c3877fc3ea2d9e6c72179d9e3ac6c2247b57ea32
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-03-14 22:18:20 +0300

    Hopefully, he problem with white spaces is solved

--- hw2_1/hw2_1/ArrayStack.cs
+++ hw2_1/hw2_1/ArrayStack.cs
@@ -2,72 +2,72 @@
 
 namespace ArrayStack
 {
-	/// <summary>
-	/// Array stack.
-	/// </summary>
-	class ArrayStack
-	{
-		/// <summary>
-		/// Max number of elements in the stack array
-		/// </summary>
-		private const int maxElementsNumber = 100; 
+    /// <summary>
+    /// Array stack.
+    /// </summary>
+    class ArrayStack
+    {
+        /// <summary>
+        /// Max number of elements in the stack array
+        /// </summary>
+        private const int maxElementsNumber = 100;
 
-		/// <summary>
-		/// Number of elements in the stack array
-		/// </summary>
-		private int currentSize = 0; 
+        /// <summary>
+        /// Number of elements in the stack array
+        /// </summary>
+        private int currentSize = 0;
 
-		/// <summary>
-		/// Stack array
-		/// </summary>
-		private int[] array = new int[maxElementsNumber]; 
+        /// <summary>
+        /// Stack array
+        /// </summary>
+        private int[] array = new int[maxElementsNumber];
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ArrayStack.ArrayStack"/> class.
-		/// </summary>
-		public ArrayStack() 
-		{
-			for (int i = 0; i < maxElementsNumber; ++i)
-			{
-				// just in case
-				this.array[i] = 0; 
-			}
-			// just in case
-			currentSize = 0; 
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArrayStack.ArrayStack"/> class.
+        /// </summary>
+        public ArrayStack()
+        {
+            for (int i = 0; i < maxElementsNumber; ++i)
+            {
+                // just in case
+                this.array[i] = 0; 
+            }
+            // just in case
+            currentSize = 0; 
+        }
 
-		/// <summary>
-		/// Returns true if stack is empty, otherwise it's false
-		/// </summary>
-		/// <returns><c>true</c>, if empty was ised, <c>false</c> otherwise.</returns>
-		public bool IsEmpty()  => this.currentSize == 0;
-		
+        /// <summary>
+        /// Returns true if stack is empty, otherwise it's false
+        /// </summary>
+        /// <returns><c>true</c>, if empty was ised, <c>false</c> otherwise.</returns>
+        public bool IsEmpty()  => this.currentSize == 0;
 
-		/// <summary>
-		/// Returns true if element is added, and false if stack is going to get overloaded
-		/// </summary>
-		/// <param name="element">Element user wants to add to the stack.</param>
-		public bool Push(int element)
-		{
-			if (currentSize < maxElementsNumber) 
-			{
-				this.array[currentSize] = element;
-				++currentSize;
-				return true;
-			} 
-			else 
-			{
-				return false;
-			}
-		}
 
-		/// <summary>
-		/// Returns the last added element, deletes it from the stack
-		/// WARNING: one should check if the stack is not empty before this operation
+        /// <summary>
+        /// Returns true if element is added, and false if stack is going to get overloaded
+        /// </summary>
+        /// <param name="element">Element user wants to add to the stack.</param>
+        public bool Push(int element)
+        {
+            if (currentSize < maxElementsNumber)
+            {
+                this.array[currentSize] = element;
+                ++currentSize;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns the last added element, deletes it from the stack
+        /// WARNING: one should check if the stack is not empty before this operation
         /// WARNING: if stack is empty, Pop() returns int.MinValue
-		/// </summary>
-		public int Pop()
-		{
+        /// </summary>
+        public int Pop()
+        {
             if (currentSize > 0)
             {
                 --currentSize;
@@ -77,6 +77,6 @@ namespace ArrayStack
             {
                 throw new Exception();
             }
-		}
-	}
+        }
+    }
 }
--- hw2_1/hw2_1/Program.cs
+++ hw2_1/hw2_1/Program.cs
@@ -2,22 +2,22 @@
 
 namespace ArrayStack
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-			ArrayStack myStack = new ArrayStack();
-			myStack.Push(1);
-			myStack.Push(2);
-			myStack.Push(3);
-			if (myStack.IsEmpty()) 
-			{
-				Console.WriteLine("The stack is empty");
-			}
-			while (!myStack.IsEmpty()) 
-			{
-				Console.WriteLine("Value in the stack: {0}", myStack.Pop());
-			}
-		}
-	}
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayStack myStack = new ArrayStack();
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            if (myStack.IsEmpty())
+            {
+                Console.WriteLine("The stack is empty");
+            }
+            while (!myStack.IsEmpty())
+            {
+                Console.WriteLine("Value in the stack: {0}", myStack.Pop());
+            }
+        }
+    }
 }
