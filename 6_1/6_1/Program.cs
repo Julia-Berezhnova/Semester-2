33eff80a72dff74a396c298968d9be95cbae0c63
Julia-Berezhnova <juliaberezhnova98@gmail.com>
2016-04-08 23:53:41 +0300

    hw6.1 is added

---
+++ 6_1/6_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class MainClass
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
